d2907b997069534a73d176c6bcc368f5868d128d
cliffbowman <github@cliffbowman.com>
2022-03-08 15:57:27 -0800

    Removed Unity IOC, using hosting extensions now.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using CommandLine;
-using Microsoft.Practices.Unity;
+﻿using CommandLine;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MullvadPinger
 {
@@ -8,18 +8,23 @@ namespace MullvadPinger
     {
         public static void Main(string[] args)
         {
-            IUnityContainer container = new UnityContainer();
-            container.RegisterType<IMullvadClient, MullvadClient>();
-            // container.RegisterType<IMullvadDataSource, MullvadDataSource>();
-            container.RegisterType<IMullvadDataSource, SampleMullvadDataSource>();
-            // container.RegisterType<IPingWrapper, PingWrapper>();
-            container.RegisterType<IPingWrapper, NoopPingWrapper>();
+            var host = Host
+                .CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddTransient<IMullvadClient, MullvadClient>();
+                    // services.AddTransient<IMullvadDataSource, MullvadDataSource>();
+                    services.AddTransient<IMullvadDataSource, SampleMullvadDataSource>();
+                    // services.AddTransient<IPingWrapper, PingWrapper>();
+                    services.AddTransient<IPingWrapper, NoopPingWrapper>();
+                })
+                .Build();
 
             Parser.Default
                 .ParseArguments<CommandLineArgs>(args)
                 .WithParsed<CommandLineArgs>(options =>
                 {
-                    var mullvadClient = container.Resolve<IMullvadClient>();
+                    var mullvadClient = host.Services.GetRequiredService<IMullvadClient>();
                     var servers = mullvadClient.GetVPNServerListAsync().GetAwaiter().GetResult();
 
                     servers.ForEach(Console.WriteLine);
